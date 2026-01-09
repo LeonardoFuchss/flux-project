199fb1ed705481af9c9c7589c4e336c0bdcfb9ab
Leonardo Fuchs <fuchsleonardo01@gmail.com>
2026-01-09 16:13:22 -0300

    feat: Setting subclass constructors.

--- src/main/java/com/leoprojects/flux/domain/transaction/expense/Expense.java
+++ src/main/java/com/leoprojects/flux/domain/transaction/expense/Expense.java
@@ -3,13 +3,13 @@ package com.leoprojects.flux.domain.transaction.expense;
 import com.leoprojects.flux.domain.transaction.Transaction;
 import jakarta.persistence.Entity;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@SuperBuilder
 public class Expense extends Transaction {
 
 }
--- src/main/java/com/leoprojects/flux/domain/transaction/income/Income.java
+++ src/main/java/com/leoprojects/flux/domain/transaction/income/Income.java
@@ -3,13 +3,13 @@ package com.leoprojects.flux.domain.transaction.income;
 import com.leoprojects.flux.domain.transaction.Transaction;
 import jakarta.persistence.Entity;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@SuperBuilder
 public class Income extends Transaction {
 
 }
